1f5f72424e6110018ddf8fec43045e47db6fb52c
Dudley <dudley.jean_pierre@student.ueh.edu.ht>
2026-07-01 14:44:54 -0400

    Add files via upload

---
+++ tontine-backend/tontine-backend/src/main/java/ht/edu/ueh/fds/tontine/TontineBackendApplication.java
@@ -0,0 +1,13 @@
+package ht.edu.ueh.fds.tontine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TontineBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TontineBackendApplication.class, args);
+	}
+
+}
---
+++ tontine-backend/tontine-backend/src/test/java/ht/edu/ueh/fds/tontine/TontineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ht.edu.ueh.fds.tontine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TontineBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
